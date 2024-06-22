c97f4f0793324324284b0e5de9194adfa1dc8f49
Waqas Ur Rehman <rehmanwaqas466@gmail.com>
2024-06-22 14:44:51 +0500

    Update: Yaml file add your post api and messaging api

--- server/message/controller.js
+++ server/message/controller.js
@@ -1,66 +1,64 @@
 const messageSchema = require("./schema");
-const conversationSchema = require("../model/conversation")
+const conversationSchema = require("../model/conversation");
 require("dotenv").config();
 
 const sendMessage = async (req, res) => {
- try {
-  const {message}=req.body;
-  console.log(message)
+  try {
+    const { message } = req.body;
+    console.log(message);
 
-  const {id: receiverId} = req.params;
-  const senderId = req.user.id;
+    const { id: receiverId } = req.params;
+    const senderId = req.user.id;
 
-  let conversation = await conversationSchema.findOne({
-    members: { $all: [senderId, receiverId] },
-  
-  });
-  console.log(conversation)
+    let conversation = await conversationSchema.findOne({
+      members: { $all: [senderId, receiverId] },
+    });
+    console.log(conversation);
 
-   if (!conversation) {
-     conversation = await conversationSchema.create({
-       members: [senderId, receiverId],
-     });
-   }
+    if (!conversation) {
+      conversation = await conversationSchema.create({
+        members: [senderId, receiverId],
+      });
+    }
 
-   const newMessage = new messageSchema({
-     senderId,
-     receiverId,
-     message,
-   });
+    const newMessage = new messageSchema({
+      senderId,
+      receiverId,
+      message,
+    });
 
-   if (newMessage) {
-     conversation.messages.push(newMessage._id);
-   }
+    if (newMessage) {
+      conversation.messages.push(newMessage._id);
+    }
 
-   await Promise.all([newMessage.save(), conversation.save()]);
-   res.status(201).json(newMessage);
-  
- } catch (error) {
-   console.log("Error to sendMessage route: ", error.message);
-   return res.status(500).json({ error: "Internal Server Error" });
- }
+    await Promise.all([newMessage.save(), conversation.save()]);
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.log("Error to sendMessage route: ", error.message);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 const getMessage = async (req, res) => {
-   try {
-     const { id: userToChatId } = req.params;
-     const senderId = req.user.id;
+  try {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user.id;
 
-     const conversation = await conversationSchema
-       .findOne({
-         members: { $all: [senderId, userToChatId] },
-       })
-       .populate("message");
+    const conversation = await conversationSchema
+      .findOne({
+        members: { $all: [senderId, userToChatId] },
+      })
+      .populate("message");
 
-       console.log(conversation)
+    console.log(conversation);
 
-     if (!conversation) return res.status(200).json([]);
+    if (!conversation) return res.status(200).json([]);
 
-     return res.status(200).json(conversation.messages);
-   } catch (error) {
-     console.log("Error to getMessages route: ", error.message);
-     res.status(500).json({ error: "Internal Server Error" });
-   }
+    return res.status(200).json(conversation.messages);
+  } catch (error) {
+    console.log("Error to getMessages route: ", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 module.exports = { sendMessage, getMessage };
