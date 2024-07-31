036fd2226c2ba1882894ced1b4e3aa5677d3e492
Waqas Ur Rehman <rehmanwaqas466@gmail.com>
2024-08-01 01:43:11 +0500

    Add: Admin panel controller, router, and also the operations added in yaml file

--- server/AdminPanel/router.js
+++ server/AdminPanel/router.js
@@ -5,13 +5,19 @@ const {
   allProducts,
   deleteUser,
   delete_Product,
+  allBookings,
+  userLength,
+  productLength,
 } = require("./controller");
 const adminProtectRoute = require("../middleware/adminProtectRoute");
 
 router.get("/allUsers", adminProtectRoute, allUsers);
+router.get("/users-length", adminProtectRoute, userLength);
 router.get("/all-Products", adminProtectRoute, allProducts);
+router.get("/products-length", adminProtectRoute, productLength);
+router.get("/all-bookings", adminProtectRoute, allBookings);
 
-router.delete("/deleteUsers", adminProtectRoute, deleteUser);
-router.delete("delete-product", adminProtectRoute, delete_Product);
+router.delete("/deleteUsers/:id", adminProtectRoute, deleteUser);
+router.delete("/delete-product/:id", adminProtectRoute, delete_Product);
 
 module.exports = router;
